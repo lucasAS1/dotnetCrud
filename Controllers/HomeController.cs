94c93c92b43afe20bae92798e1437c7d814e0ec0
lucasAS1 <lucas.a.silva1@outlook.com>
2021-10-14 18:38:07 -0300

    Cadastro inserido

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Crud.Controllers
             return View();
         }
 
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
